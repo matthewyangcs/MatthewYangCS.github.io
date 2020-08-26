8049e58c47e895fd82659b3ae004fa6916b7a569
Matthew Yang <mattyang@host-68-234-129-63.gaatuhg.atlanta.ga.us.clients.pavlovmedia.net>
2020-08-26 02:10:12 -0400

    New Website

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>personal website for patrick cao</h1>
+
+<a>https://prcao.github.io/</a>
---
+++ js/scrolling.js
@@ -0,0 +1,9 @@
+const options = {
+    duration: 1000,
+    scale: 1,
+    easing: 'ease'
+
+};
+
+window.sr = ScrollReveal();
+sr.reveal('.revealable', options);
